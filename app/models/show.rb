719e8a98fdaba1d267be7d8b3bfb7600597e02fc
Alex Zhang <alexgzhang92@gmail.com>
2018-06-15 21:13:23 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -12,7 +12,7 @@ class Show < ActiveRecord::Base
   end
   
   def Show::least_popular_show
-    self.find_by(rating, lowest_rating)
+    self.find_by("rating, lowest_rating")
   end
   
   def Show::ratings_sum
