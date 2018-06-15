e89578e99b86b315eafbe4b2c1a62713aeb68040
Alex Zhang <alexgzhang92@gmail.com>
2018-06-15 20:59:13 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,29 +1,29 @@
 class Show < ActiveRecord::Base
-  def highest_rating
+  def Show::highest_rating
     self.class.maximum("rating")
   end
   
-  def most_popular_show
+  def Show::most_popular_show
     self.class.find_by(highest_rating)
   end
   
-  def lowest_rating
+  def Show::lowest_rating
     self.class.minimum("rating")
   end
   
-  def least_popular_show
+  def Show::least_popular_show
     self.class.find_by(lowest_rating)
   end
   
-  def ratings_sum
+  def Show::ratings_sum
     self.class.sum("ratings")
   end
   
-  def popular_shows
+  def Show::popular_shows
     self.class.where("rating > ?", 5)
   end
   
-  def shows_by_alphabetical_order
+  def Show::shows_by_alphabetical_order
     self.class.order("ASC")
   end
 end
